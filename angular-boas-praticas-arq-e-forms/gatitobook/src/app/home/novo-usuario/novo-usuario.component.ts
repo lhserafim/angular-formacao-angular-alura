6eef04033194b15891105f88f13d951f03d3f20f
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-07-09 11:42:18 -0300

    Exploramos o tópico de validações em formulário reativos no Angular. e aprendemos como aplicar validações comuns e padronizadas no nosso componente do nosso formulário, além de usar a classe utilitária Validators, padrão do Angular. Criamos uma validação customizada para um campo e vimos quais são os requisitos para uma função ser reconhecida como validação no Angular. Também trabalhamos em uma função de validação que avaliava mais do que um campo do nosso formulário. Por fim, criamos uma validação que consulta o backend da nossa aplicação e aprendemos sobre os operadores RXJS, uma biblioteca poderosa que o Angular utiliza.

--- angular-boas-praticas-arq-e-forms/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ angular-boas-praticas-arq-e-forms/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { UsuarioExisteService } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,7 +19,8 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder, 
     private novoUsuarioService: NovoUsuarioService,
-    private usuarioExistenteService: UsuarioExisteService) { }
+    private usuarioExistenteService: UsuarioExisteService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
@@ -25,15 +28,28 @@ export class NovoUsuarioComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator], [this.usuarioExistenteService.usuarioJaExiste()]],  // IMPORTANTE! O array de validação assíncrona precisa estar na terceira posição
       password: ['']
-    })
+    },
+    {
+      validators: [usuarioSenhaIguaisValidator],
+    });
   }
 
   // Este método será executado no evento de onSubmit do formulário
   cadastrar() {
-    // getRawValue gera um objeto com o estado das variáveis do novo formulário
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    // as NovoUsuario é um cast e só posso fazê-lo pois os meus elementos de novoUsuarioForm são idênticos a NovoUsuario.ts
-    console.log(novoUsuario);
+    if (this.novoUsuarioForm.valid) {
+      // getRawValue gera um objeto com o estado das variáveis do novo formulário
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario; // as NovoUsuario é um cast e só posso fazê-lo pois os meus elementos de novoUsuarioForm são idênticos a NovoUsuario.ts
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(
+        () => {
+          this.router.navigate(['']);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+    }
+    
+
   }
 
 }
